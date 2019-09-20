c5c9460a13ae618c658f0866ab60d35c12dcfac4
metastable-void <git@yuka.h09.eu>
2019-09-20 19:49:02 +0900

    update

--- app/src/main/java/org/menhera/spotnotes/RegisterActivity.java
+++ app/src/main/java/org/menhera/spotnotes/RegisterActivity.java
@@ -5,12 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class RegisterActivity extends AppCompatActivity {
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    DatePickerDialog picker;
+    TimePickerDialog timePicker;
+    Button regDateButton;
+    Button regTimeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +35,46 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Add Reminder");
+
+        final Calendar cldr = Calendar.getInstance();
+        day = cldr.get(Calendar.DAY_OF_MONTH);
+        month = cldr.get(Calendar.MONTH);
+        year = cldr.get(Calendar.YEAR);
+
+        regDateButton = findViewById(R.id.regDateButton);
+        regDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                picker = new DatePickerDialog(RegisterActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year2, int monthOfYear, int dayOfMonth) {
+                                day = dayOfMonth;
+                                month = monthOfYear;
+                                year = year2;
+                                regDateButton.setText(year + "-" + month + "-" + day);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        regTimeButton = findViewById(R.id.regTimeButton);
+        regTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePicker = new TimePickerDialog(RegisterActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker tp, int sHour, int sMinute) {
+                                hour = sHour;
+                                minute = sMinute;
+                                regTimeButton.setText(hour + ":" + minute);
+                            }
+                        }, hour, minute, true);
+                timePicker.show();
+            }
+        });
     }
 
     @Override
