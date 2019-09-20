0f6b5ff0dc79608c27a036cc0b87d40c85d56743
metastable-void <git@yuka.h09.eu>
2019-09-21 08:24:14 +0900

    update

--- app/src/main/java/org/menhera/spotnotes/RecordItem.java
+++ app/src/main/java/org/menhera/spotnotes/RecordItem.java
@@ -4,9 +4,40 @@ public class RecordItem extends ListItem {
     private String title = "";
     private String avgDuration = "";
 
+    final public int REPEAT_NONE = 0;
+    final public int REPEAT_EVERYDAY = 1;
+    final public int REPEAT_WEEK = 2;
+    final public int REPEAT_MONTH = 3;
+    final public int REPEAT_YEAR = 4;
+
+    private int repeat = REPEAT_NONE;
+
     public RecordItem(String title, String avgDuration) {
         this.title = title;
         this.avgDuration = avgDuration;
+    }
+
+    public void setRepeat (int repeat) {
+        switch (repeat) {
+            case REPEAT_EVERYDAY:
+                this.repeat = REPEAT_EVERYDAY;
+                break;
+
+            case REPEAT_WEEK:
+                this.repeat = REPEAT_WEEK;
+                break;
+
+            case REPEAT_MONTH:
+                this.repeat = REPEAT_MONTH;
+                break;
+
+            case REPEAT_YEAR:
+                this.repeat = REPEAT_YEAR;
+                break;
+
+            default:
+                this.repeat = REPEAT_NONE;
+        }
     }
 
     public String getTitle () {
